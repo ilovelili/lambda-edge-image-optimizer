596e63b1c6f2836813b5191d443ca810867918b7
ilovelili <route666@live.cn>
2020-02-26 14:10:39 +0800

    working version

--- lambda/origin-response-function/index.js
+++ lambda/origin-response-function/index.js
@@ -1,167 +1,97 @@
 "use strict";
 
-const querystring = require("querystring");
-const aws = require("aws-sdk");
-const s3 = new aws.S3({
-  // region: process.env.REGION, env variables not supported by lambda edge
-  region: "us-east-1",
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3({
+	region: "us-east-1",
 	signatureVersion: "v4"
 });
-const sharp = require("sharp");
+const Sharp = require("sharp");
 
-// Image types that can be handled by Sharp
-const supportImageTypes = ["jpg", "jpeg", "png", "gif", "webp", "svg", "tiff"];
+// set the S3 and API GW endpoints
+const BUCKET = "digitalkaleido-static-hosting";
 
-exports.handler = async (event, context, callback) => {
-	const { config, request, response } = event.Records[0].cf;
-
-	// select origin bucket
-	const bucket = "digitalkaleido-static-hosting";
-
-	// check if image is present and not cached.
-	if (response.status == 200) {
-		// parse the querystring key-value pairs.
-		const params = querystring.parse(request.querystring);
-		// If none of the s, t, or q variables is present, just pass the request
-		if (!params.s || !params.t || !params.q) {
-			callback(null, response);
-			return;
-		}
+exports.handler = (event, context, callback) => {
+	let response = event.Records[0].cf.response;
+	//check if image is not present
+	if (response.status == 404) {
+		let request = event.Records[0].cf.request;
+		// read the required path. Ex: uri /images/100x100/webp/image.jpg
+		let path = request.uri;
 
 		// read the S3 key from the path variable.
-		// assets/images/sample.jpeg
-		let key = decodeURIComponent(request.uri).substring(1);
+		// Ex: path variable /images/100x100/webp/image.jpg
+		// try with https://cdn.digitalkaleido.com/101x101/jpeg/logo.png
+		let key = path.substring(1);
 
-		// parse the width, height, type, quality, format, image name
-		let width, height, type, quality, requiredFormat;
-
-		// s=100x100&t=enlargement&q=100(&f=webp)
-		const sizeMatch = params.s.split("x");
-		const typeMatch = params.t;
-		const qualityMatch = params.q;
-		const formatMatch = params.f;
-
-		let originalFormat = key.match(/(.*)\.(.*)/)[2].toLowerCase();
-
-		if (
-			!supportImageTypes.some(type => {
-				return type == originalFormat;
-			})
-		) {
-			responseUpdate(403, "Forbidden", "Unsupported image type", [
-				{ key: "Content-Type", value: "text/plain" }
-			]);
-			callback(null, response);
-			return;
-		}
-
-		width = parseInt(sizeMatch[0], 10);
-		height = parseInt(sizeMatch[1], 10);
-		type = typeMatch == "crop" ? "cover" : typeMatch;
-		quality = parseInt(qualityMatch, 10);
-
-		// correction for jpg required for 'Sharp'
-		originalFormat = originalFormat == "jpg" ? "jpeg" : originalFormat;
-		requiredFormat =
-			formatMatch == "webp"
-				? "webp"
-				: originalFormat == "jpg"
-				? "jpeg"
-				: originalFormat;
+		// parse the prefix, width, height and image name
+		// Ex: key=images/200x200/webp/image.jpg
+		let prefix, originalKey, match, width, height, requiredFormat, imageName;
 
 		try {
-			// get the source image file
-			const s3Object = await s3
-				.getObject({
-					Bucket: bucket,
-					Key: key
-				})
-				.promise();
-			if (s3Object.ContentLength == 0) {
-				responseUpdate(404, "Not Found", "The image does not exist.", [
-					{ key: "Content-Type", value: "text/plain" }
-				]);
-				callback(null, response);
-				return;
-			}
+			match = key.match(/(.*)\/(\d+)x(\d+)\/(.*)\/(.*)/);
+			prefix = match[1];
+			width = parseInt(match[2], 10);
+			height = parseInt(match[3], 10);
 
-			let metaData,
-				resizedImage,
-				byteLength = 0;
-
-			if (requiredFormat != "jpeg" && requiredFormat != "webp") {
-				console.log(`Info: image format is ${requiredFormat}, trying to jpeg.`);
-				requiredFormat = "jpeg";
-			}
-			while (1) {
-				resizedImage = await sharp(s3Object.Body).rotate();
-				metaData = await resizedImage.metadata();
-
-				if (metaData.width > width || metaData.height > height) {
-					resizedImage.resize(width, height, { fit: type });
-				}
-				if (byteLength >= 1046528 || originalFormat != requiredFormat) {
-					resizedImage.toFormat(requiredFormat, { quality: quality });
-				}
-				resizedImage = await resizedImage.toBuffer();
-
-				byteLength = Buffer.byteLength(resizedImage, "base64");
-				if (byteLength == metaData.size) {
-					callback(null, response);
-					return;
-				}
-				if (byteLength >= 1046528) {
-					quality -= 10;
-					console.log(
-						`Info: Content-Length is ${byteLength}, ` +
-							`trying again with quality ${quality}. // ` +
-							bucket +
-							" ## " +
-							key
-					);
-				} else {
-					break;
-				}
-			}
-
-			responseUpdate(
-				200,
-				"OK",
-				resizedImage.toString("base64"),
-				[{ key: "Content-Type", value: "image/" + requiredFormat }],
-				"base64"
-			);
-			response.headers["cache-control"] = [
-				{ key: "cache-control", value: "max-age=31536000" }
-			];
-			return callback(null, response);
+			// correction for jpg required for 'Sharp'
+			requiredFormat = match[4] == "jpg" ? "jpeg" : match[4];
+			imageName = match[5];
+			originalKey = prefix + "/" + imageName;
 		} catch (err) {
-			console.log(
-				`for debugging. key, params === ${key} === q=${params.q}&s=${params.s}&t=${params.t}`
-			);
-			console.error(err);
-			callback(null, response);
-			return;
+			// no prefix exist for image..
+			console.log("no prefix present..");
+			match = key.match(/(\d+)x(\d+)\/(.*)\/(.*)/);
+			width = parseInt(match[1], 10);
+			height = parseInt(match[2], 10);
+
+			// correction for jpg required for 'Sharp'
+			requiredFormat = match[3] == "jpg" ? "jpeg" : match[3];
+			imageName = match[4];
+			originalKey = imageName;
 		}
-	} else {
+
+		// get the source image file
+		S3.getObject({ Bucket: BUCKET, Key: originalKey })
+			.promise()
+			// perform the resize operation
+			.then(data =>
+				Sharp(data.Body)
+					.resize(width, height)
+					.toFormat(requiredFormat)
+					.toBuffer()
+			)
+			.then(buffer => {
+				// save the resized object to S3 bucket with appropriate object key.
+				S3.putObject({
+					Body: buffer,
+					Bucket: BUCKET,
+					ContentType: "image/" + requiredFormat,
+					CacheControl: "max-age=31536000",
+					Key: key,
+					StorageClass: "STANDARD"
+				})
+					.promise()
+					// even if there is exception in saving the object we send back the generated
+					// image back to viewer below
+					.catch(() => {
+						console.log("Exception while writing resized image to bucket");
+					});
+
+				// generate a binary response with resized image
+				response.status = 200;
+				response.body = buffer.toString("base64");
+				response.bodyEncoding = "base64";
+				response.headers["content-type"] = [
+					{ key: "Content-Type", value: "image/" + requiredFormat }
+				];
+				callback(null, response);
+			})
+			.catch(err => {
+				console.log("Exception while reading source image :%j", err);
+			});
+	} // end of if block checking response statusCode
+	else {
 		// allow the response to pass through
 		callback(null, response);
-		return;
-	}
-
-	function responseUpdate(
-		status,
-		statusDescription,
-		body,
-		contentHeader,
-		bodyEncoding = undefined
-	) {
-		response.status = status;
-		response.statusDescription = statusDescription;
-		response.body = body;
-		response.headers["content-type"] = contentHeader;
-		if (bodyEncoding) {
-			response.bodyEncoding = bodyEncoding;
-		}
 	}
 };
